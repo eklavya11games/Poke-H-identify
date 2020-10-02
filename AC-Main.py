cdf94f0758713b9a6a709200d7b2a8cd14bbbe66
William Greenlee <greenlee04@gmail.com>
2020-10-01 21:27:36 -0400

    Update 1.0.3-final
    Added support for some versions of windows, and minor improvements for some pokemon recognition.

--- AC-Main.py
+++ AC-Main.py
@@ -6,7 +6,7 @@ from discord import Client
 
 import pokeformat
 
-__version__ = "v1.0.2rc1"
+__version__ = "v1.0.3-final"
 
 
 def main():
@@ -19,14 +19,13 @@ def main():
                 pf = json.load(f)
             return pf
         pokeformat.format_poke()
+        read_pokemon()
 
     client: Client = Client()
     pokemon: [str] = read_pokemon()
 
     @client.event
     async def on_ready():
-        if not os.path.exists('lts/'):
-            os.mkdir(Path('lts/'))
         print(f'Initialized PokeHelper {__version__}')
 
     @client.event
@@ -34,15 +33,18 @@ def main():
         if msg.author.id in bots:
             print("heard bot message")
             if 'The pokémon is' in msg.content:
-                content = str(msg.content).strip(' ').strip('.').split(' ')
+                content = str(msg.content).strip().strip('.').split(' ')
                 pokemon_hint = ''
                 for msg_piece in content:
                     if '_' in msg_piece:
+                        if 'é' in msg_piece:
+                            await msg.channel.send('Flabébé')
+                            return
                         msg_piece = msg_piece.replace('\\', '')
                         pokemon_hint += (' ' + msg_piece)
                         pokemon_hint = pokemon_hint.strip()
                 final_mons = search_mons(pokemon_hint)
-                await msg.channel.send(str(final_mons))
+                await msg.channel.send(str(final_mons).strip('_'))
 
     def search_mons(hint) -> [str]:
         possible_mons = pokemon
